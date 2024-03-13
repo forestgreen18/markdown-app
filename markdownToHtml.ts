13d8f2760ad05414026b1404d185344fc0e04dd0
Illia Kornyk <forest.bukson@gmail.com>
2024-03-13 19:11:54 +0200

    refactor: refactor markdown formatting functions for improved readability and maintainability

--- markdownToHtml.ts
+++ markdownToHtml.ts
@@ -5,58 +5,65 @@ const formatTag = (html: string): string => {
 
 // function for REGEXP to format code-block, highlight remarks/keywords
 const formatCode = (m: string, p1: string, p2: string): string => {
-  p2 = p2.replace(/</g, "<").replace(/\>/g, ">").replace(/\t/g, "   ");
+  const keywordRegex =
+    /(\s)(function|return|var|let|const|if|then|else|elseif|end|for|next|do|while|loop|continue|break|case|switch|try|catch|finally)(\s)/gim;
+  const commentRegex = /^\/\/(.*)/gm;
+  const commentSpaceRegex = /\s\/\/(.*)/gm;
+
   p2 = p2
-    .replace(/^\/\/(.*)/gm, "<rem>//$1</rem>")
-    .replace(/\s\/\/(.*)/gm, " <rem>//$1</rem>");
-  p2 = p2.replace(
-    /(\s)(function|return|var|let|const|if|then|else|elseif|end|for|next|do|while|loop|continue|break|case|switch|try|catch|finally)(\s)/gim,
-    "$1<b>$2</b>$3"
-  );
-  return "<pre" + p1 + "><code>" + p2 + "</code></pre>";
+    .replace(/</g, "<")
+    .replace(/\>/g, ">")
+    .replace(/\t/g, "   ")
+    .replace(commentRegex, "<rem>//$1</rem>")
+    .replace(commentSpaceRegex, " <rem>//$1</rem>")
+    .replace(keywordRegex, "$1<b>$2</b>$3");
+
+  return `<pre${p1}><code>${p2}</code></pre>`;
 };
 
 // function to convert mdString into HTML string
 const formatMD = (mdstr: string): string => {
+  const hrRegex = /^-{3,}|^\_{3,}|^\*{3,}$/gm;
+  const codeBlockRegex = /``(.*?)``/gm;
+  const blockquoteRegex = /^\>\> (.*$)/gm;
+  const textDecoRegex = /\*\*\*(\w.*?[^\\])\*\*\*/gm;
+  const indentRegex = /^ {4,10}(.*)/gm;
+
   // horizontal rule => <hr>
-  mdstr = mdstr
-    .replace(/^-{3,}|^\_{3,}|^\*{3,}$/gm, "<hr>")
-    .replace(/\n\n<hr\>/g, "\n<br><hr>");
+  mdstr = mdstr.replace(hrRegex, "<hr>").replace(/\n\n<hr\>/g, "\n<br><hr>");
 
   // inline code-block: `code-block` => <code>code-block</code>
-  mdstr = mdstr.replace(/``(.*?)``/gm, function (m: string, p: string): string {
-    return "<code>" + formatTag(p).replace(/`/g, "&#96;") + "</code>";
+  mdstr = mdstr.replace(codeBlockRegex, (m: string, p: string): string => {
+    return `<code>${formatTag(p).replace(/`/g, "`")}</code>`;
   });
   mdstr = mdstr.replace(/`(.*?)`/gm, "<code>$1</code>");
 
   // blockquote, max 2 levels => <blockquote>{text}</blockquote>
-  mdstr = mdstr.replace(
-    /^\>\> (.*$)/gm,
-    "<blockquote><blockquote>$1</blockquote></blockquote>"
-  );
-  mdstr = mdstr.replace(/^\> (.*$)/gm, "<blockquote>$1</blockquote>");
-  mdstr = mdstr.replace(/<\/blockquote\>\n<blockquote\>/g, "\n<br>");
-  mdstr = mdstr.replace(/<\/blockquote\>\n<br\><blockquote\>/g, "\n<br>");
+  mdstr = mdstr
+    .replace(
+      blockquoteRegex,
+      "<blockquote><blockquote>$1</blockquote></blockquote>"
+    )
+    .replace(/^\> (.*$)/gm, "<blockquote>$1</blockquote>")
+    .replace(/<\/blockquote\>\n<blockquote\>/g, "\n<br>")
+    .replace(/<\/blockquote\>\n<br\><blockquote\>/g, "\n<br>");
 
   // text decoration: bold, italic, underline, strikethrough, highlight
-  mdstr = mdstr.replace(/\*\*\*(\w.*?[^\\])\*\*\*/gm, "<b><em>$1</em></b>");
-  mdstr = mdstr.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
-  mdstr = mdstr.replace(/_(.*?)_/g, "<em>$1</em>");
-  mdstr = mdstr.replace(/___(\w.*?[^\\])___/gm, "<b><em>$1</em></b>");
-  mdstr = mdstr.replace(/__(\w.*?[^\\])__/gm, "<u>$1</u>");
-  // mdstr = mdstr.replace(/_(\w.*?[^\\])_/gm, '<u>$1</u>')  // NOT support!!
-  mdstr = mdstr.replace(/~~(\w.*?)~~/gm, "<del>$1</del>");
-  mdstr = mdstr.replace(/\^\^(\w.*?)\^\^/gm, "<ins>$1</ins>");
+  mdstr = mdstr
+    .replace(textDecoRegex, "<b><em>$1</em></b>")
+    .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
+    .replace(/_(.*?)_/g, "<em>$1</em>")
+    .replace(/___(\w.*?[^\\])___/gm, "<b><em>$1</em></b>")
+    .replace(/__(\w.*?[^\\])__/gm, "<u>$1</u>")
+    .replace(/~~(\w.*?)~~/gm, "<del>$1</del>")
+    .replace(/\^\^(\w.*?)\^\^/gm, "<ins>$1</ins>");
 
   // indent as code-block
-  mdstr = mdstr.replace(
-    /^ {4,10}(.*)/gm,
-    function (m: string, p: string): string {
-      return "<pre><code>" + formatTag(p) + "</code></pre>";
-    }
-  );
-  mdstr = mdstr.replace(/^\t(.*)/gm, function (m: string, p: string): string {
-    return "<pre><code>" + formatTag(p) + "</code></pre>";
+  mdstr = mdstr.replace(indentRegex, (m: string, p: string): string => {
+    return `<pre><code>${formatTag(p)}</code></pre>`;
+  });
+  mdstr = mdstr.replace(/^\t(.*)/gm, (m: string, p: string): string => {
+    return `<pre><code>${formatTag(p)}</code></pre>`;
   });
   mdstr = mdstr.replace(/<\/code\><\/pre\>\n<pre\><code\>/g, "\n");
 
@@ -69,17 +76,21 @@ export const simpleMarkdown = (mdText: string): string => {
   let pos1: number = 0,
     pos2: number = 0,
     mdHTML: string = "";
-  mdText = mdText.replace(/\r\n/g, "\n").replace(/\n~~~/g, "\n```");
-  mdText = mdText.replace(/\n``` *(.*?)\n([\s\S]*?)\n``` *\n/g, formatCode);
+  let modifiedMdText = mdText.replace(/\r\n/g, "\n").replace(/\n~~~/g, "\n```");
+  modifiedMdText = modifiedMdText.replace(
+    /\n``` *(.*?)\n([\s\S]*?)\n``` *\n/g,
+    formatCode
+  );
 
   // split by "<code>", skip for code-block and process normal text
-  while ((pos1 = mdText.indexOf("<code>")) >= 0) {
-    pos2 = mdText.indexOf("</code>", pos1);
-    mdHTML +=
-      formatMD(mdText.slice(0, pos1)) +
-      mdText.slice(pos1 + 6, pos2 > 0 ? pos2 : mdText.length);
-    mdText = mdText.slice(pos2 + 7);
+  while ((pos1 = modifiedMdText.indexOf("<code>")) >= 0) {
+    pos2 = modifiedMdText.indexOf("</code>", pos1);
+    mdHTML += `${formatMD(modifiedMdText.slice(0, pos1))}${modifiedMdText.slice(
+      pos1 + 6,
+      pos2 > 0 ? pos2 : modifiedMdText.length
+    )}`;
+    modifiedMdText = modifiedMdText.slice(pos2 + 7);
   }
 
-  return mdHTML + formatMD(mdText);
+  return `${mdHTML}${formatMD(modifiedMdText)}`;
 };
