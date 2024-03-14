169627d78f71769b32db11fe65b8a1504b551d2c
Illia Kornyk <forest.bukson@gmail.com>
2024-03-14 10:28:57 +0200

    feat: handle cli flags

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
 import { readFileContent } from "./fileReader";
 import { simpleMarkdown } from "./markdownToHtml";
+import { writeFileSync } from "fs";
 
 async function main() {
   try {
-    const filePath = "./test-markdown.md"; // Replace with the actual file path
-    const content = await readFileContent(filePath); // Display the file content
-    console.log(simpleMarkdown(content));
+    // process.argv[2] will be the path to the input file
+    const filePath = process.argv[2];
+    const content = await readFileContent(filePath);
+
+    // process.argv[3] will be the '--out' flag
+    // process.argv[4] will be the path to the output file
+
+    if (process.argv[3] === "--out") {
+      const outputPath = process.argv[4];
+      const htmlContent = simpleMarkdown(content);
+      writeFileSync(outputPath, htmlContent);
+      process.exit(0);
+    } else if (process.argv[3]) {
+      throw new Error(`Unrecognized flag: ${process.argv[3]}`);
+    } else {
+      console.log(simpleMarkdown(content));
+      process.exit(0);
+    }
   } catch (error) {
-    console.error("Error reading file:", error);
+    console.error("Error:", error);
+    process.exit(1);
   }
 }
 
