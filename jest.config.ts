353bf1d7ff018c2cbb61fa812348bee06a6cc83e
Illia Kornyk <forest.bukson@gmail.com>
2024-04-21 20:13:34 +0300

    fix: update jest config

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: { '^.+\\.tsx?$': 'ts-jest' }
+};
+
+export default config;
