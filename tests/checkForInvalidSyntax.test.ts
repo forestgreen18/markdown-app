d0de63cdb375cb14992e3926f8647f27a9ce40f8
Illia Kornyk <forest.bukson@gmail.com>
2024-04-22 22:09:43 +0300

    fix: take invalid md from ts const

--- tests/checkForInvalidSyntax.test.ts
+++ tests/checkForInvalidSyntax.test.ts
@@ -1,24 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { checkForInvalidSyntax } = require('../src/converters/markdownToHtml');
 const fs = require('fs');
+const { testCases } = require('../testInputFiles/testCases');
 const { getTestFilePath } = require('../utils/getTestFilePath');
+
+const { checkForInvalidSyntax } = require('../src/converters/markdownToHtml');
 
 describe('checkForInvalidSyntax', () => {
   it('identifies invalid markdown syntax', () => {
-    const mdFilePath = getTestFilePath('invalidMD.md');
-    const mdContent = fs.readFileSync(mdFilePath, 'utf8');
-
-    // Log the mdContent
-    console.log('mdContent:', mdContent);
-
-    const result = checkForInvalidSyntax(mdContent);
-    // Log the result of the checkForInvalidSyntax function
-    console.log('checkForInvalidSyntax result:', result);
+    console.log(testCases);
+    const mdContent = testCases.invalidMarkdown;
 
     const expectedOutput = {
-      '**_invalid syntax_**':
-        'Embedding styles within each other is prohibited',
-      '_`invalid code`_': 'Embedding styles within each other is prohibited'
+      '**_invalid syntax_**': 'Embedding styles within each other is prohibited'
     };
 
     expect(checkForInvalidSyntax(mdContent)).toEqual(expectedOutput);
